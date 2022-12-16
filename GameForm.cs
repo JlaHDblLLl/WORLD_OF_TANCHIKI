fef1bad44e9b3cb0043642e433dc378e07c1959b
JlaHDblLLl <manex.gamestrtrpo@gmail.com>
2022-12-16 19:15:30 +0300

    Update GameForm.cs

--- GameForm.cs
+++ GameForm.cs
@@ -14,20 +14,12 @@ namespace Game
 {
     public partial class GameForm : Form
     {
-        private bool read;
-        private bool write;
-        private bool firstTurn = true;
         private GamePanel gp;
         public Thread t;
-        private bool communicating = true;
-        private TcpClient communicator;
         public StreamReader sr;
         public StreamWriter sw;
-        IntroForm IntroForm;
-        String[] powAngle;
-        User user;
 
-        public GameForm(IntroForm introform, User user)       //SinglePlayer Constructor
+        public GameForm(IntroForm introform, User user)       
         {
             InitializeComponent();
             this.gp = new GamePanel(user,Mode.Single);
@@ -36,111 +28,5 @@ namespace Game
             Controls.Add(gp);
             gp.StartGame();
         }
-        public GameForm(int myPortNumber, IntroForm introForm)//Host Constructor
-        {
-            InitializeComponent();
-            this.IntroForm = introForm;
-            this.gp = new GamePanel(User.Host,Mode.Multi);
-            Controls.Add(gp);
-            gp.StartGame();
-            user = User.Host;
-            initHost_Communication(myPortNumber);
-            write = true;
-            read = false;
-        }
-        public GameForm(String hostIP, int hostPortNumber, IntroForm introForm)      //Guest Constructor
-        {
-            InitializeComponent();
-            this.IntroForm = introForm;
-            gp = new GamePanel(User.Client,Mode.Multi);
-            Controls.Add(gp);
-            gp.StartGame();
-            user = User.Client;
-            initClient_Communication(hostIP, hostPortNumber);
-            write = false;
-            read = true;
-        }
-
-        private void initHost_Communication(int myPortNumber)
-        {
-            TcpListener listener = TcpListener.Create(myPortNumber);
-            listener.Start();
-            communicator = listener.AcceptTcpClient();
-            Console.WriteLine("Communication accepted");
-            sr = new StreamReader(communicator.GetStream());
-            sw = new StreamWriter(communicator.GetStream());
-            t = new Thread(communicate);
-            t.Start();
-            listener.Stop();
-        }
-
-        private void initClient_Communication(String hostIP, int hostPortNumber)
-        {
-            communicator = new TcpClient(hostIP, hostPortNumber);
-            sr = new StreamReader(communicator.GetStream());
-            sw = new StreamWriter(communicator.GetStream());
-            t = new Thread(communicate);
-            t.Start();
-        }
-
-        private void communicate()
-        {
-            while (communicating)
-            {               
-                if (write)
-                {
-                    if ((gp.getPlayerAnglePower() != null))
-                    {
-                        sw.WriteLine(gp.getPlayerAnglePower());
-                        sw.Flush();
-                        read = true;
-                        write = false;
-                    }
-                    else if(gp.isGameEnd )
-                    {
-                        sw.WriteLine("GameEnded");
-                        sw.Flush();
-                        read = false;
-                        write = false;
-                        communicating = false;
-
-                    }
-                }
-                if (read )
-                {
-                    string request = sr.ReadLine();
-                    if (request != null && request!="GameEnded")
-                    {
-                        string[] tokens = request.Split(' ');
-                        int angle = Convert.ToInt32(tokens[0]);
-                        double power = Convert.ToDouble(tokens[1]);
-                        gp.setOpponentAnglePower(angle, power);
-                        read = false;
-                        write = true;
-                    }
-                    else if(request=="GameEnded")
-                    {
-                        read = false;
-                        write = false;
-                        communicating = false;
-                    }
-                }
-                
-            }
-            EndGame();
-        }
-        private void EndGame()
-        {
-            sr.Close();
-            sw.Close();
-            communicator.Close();
-            communicating = false;
-            write = false;
-            read = false;
-            this.Close();
-            IntroForm.Show();
-            t.Abort();
-        }
-
     }
 }
